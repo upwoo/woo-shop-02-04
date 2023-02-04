d0b5f3375955fb34ebbca3e64aff97bca950b8ac
Максим Борисенко <maksimborisenko@Maksims-MacBook-Pro.local>
2023-02-04 15:39:06 +0300

    Add footer HTML and add file IMG and app.js

--- footer.php
+++ footer.php
@@ -9,7 +9,81 @@
  * @package estore
  */
 ?>
-
+<footer>
+    <div class="container">
+        <div class="wrapper flex">
+            <div class="logo">
+                <a href="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="">
+                </a>
+            </div>
+            <div class="menu flex al-center sp-between">
+                <div class="item">
+                    <div class="title">
+                        <span>
+                            Каталог
+                        </span>
+                    </div>
+                    <ul class="item-s">
+                        <li>
+                            <a href="">
+                                Плодовые
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Многолетнии
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="item">
+                    <div class="title">
+                        <span>
+                            Услуги
+                        </span>
+                    </div>
+                    <ul class="item-s">
+                        <li>
+                            <a href="">
+                                Плодовые
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Многолетнии
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="item">
+                    <div class="title">
+                        <span>
+                            Информация
+                        </span>
+                    </div>
+                    <ul class="item-s">
+                        <li>
+                            <a href="">
+                                Доставка и оплата
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Согласие на обработку данных
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Статьи и новости
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 
 <?php wp_footer(); ?>
