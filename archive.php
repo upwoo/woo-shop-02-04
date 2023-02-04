8c5610dc79654d7e97a85623358ff4807f34acec
Максим Борисенко <maksimborisenko@Maksims-MacBook-Pro.local>
2023-02-04 16:18:46 +0300

    add archive and content-post

--- archive.php
+++ archive.php
@@ -7,40 +7,63 @@
  * @package estore
  */
 get_header(); ?>
-	<div class="container single-section">
-		<div class="w3ls_mobiles_grids">
-			<div id="primary" class="content-area col-md-8 w3ls_mobiles_grid_left">
-				<main id="main" class="site-main">
-					
-					<?php
-					if ( have_posts() ) : ?>
-						
-						<header class="page-header">
-							<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="archive-description">', '</div>' );
-							?>
-						</header><!-- .page-header -->
-						
-						<?php
-						/* Start the Loop */
-						while ( have_posts() ) : the_post();
-							/*
-							 * Include the Post-Format-specific template for the content.
-							 * If you want to override this in a child theme, then include a file
-							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							 */
-							get_template_part( 'template-parts/content', get_post_format() );
-						endwhile;
-						the_posts_navigation();
-					else :
-						get_template_part( 'template-parts/content', 'none' );
-					endif; ?>
-				
-				</main><!-- #main -->
-			</div><!-- #primary -->
-			<?php get_sidebar(); ?>
-		</div>
-	</div>
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="wrapper">
+                <ul class="crumbs">
+                    <li><a href="index.html">Главная</a></li>
+                    <li> / </li>
+                    <li><a href="" class="active">Блог</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-in">
+        <div class="container">
+            <div class="title">
+                <h2>Блог</h2>
+            </div>
+            <div class="wrapper flex sp-between al-center wrap">
+                <?php
+                if ( have_posts() ) : ?>
+
+                    <header class="page-header">
+                        <?php
+                        the_archive_title( '<h1 class="page-title">', '</h1>' );
+                        the_archive_description( '<div class="archive-description">', '</div>' );
+                        ?>
+                    </header><!-- .page-header -->
+
+                    <?php
+                    /* Start the Loop */
+                    while ( have_posts() ) : the_post();
+                        /*
+                         * Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        get_template_part( 'template-parts/content', get_post_format() );
+                    endwhile;
+                    the_posts_navigation();
+                else :
+                    get_template_part( 'template-parts/content', 'none' );
+                endif; ?>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="pagi">
+            <ul class="pagi">
+                <li class="current"><a href="">1</a></li>
+                <li><a href="">2</a></li>
+                <li><a href="">3</a></li>
+                <li><a href="">4</a></li>
+                <li><a href="">5</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
 <?php
 get_footer();
---
+++ template-parts/content-post.php
@@ -0,0 +1,13 @@
+<a href="" class="item-permalink">
+    <div class="single-img__item">
+        <img src="img/item-single-1.png" alt="">
+    </div>
+    <div class="single-title__item">
+        <span><?php the_title(); ?></span>
+    </div>
+    <div class="single-description__item">
+        <p>
+            <?php the_excerpt(); ?>
+        </p>
+    </div>
+</a>
